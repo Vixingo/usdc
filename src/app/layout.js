a90365e6af4b5b50bddc0d9dcbf925ebd9b09395
Vixingo <nazmul.abbu@gmail.com>
2024-07-29 01:08:57 +0600

    latest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { EdgeStoreProvider } from "../lib/edgestore";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <EdgeStoreProvider>{children}</EdgeStoreProvider>
-            </body>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
